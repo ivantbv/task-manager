7b1ebb3126517af4f313bd3d3f66b9aa583bc968
Ivan <ivantbv@gmail.com>
2020-11-19 21:03:54 +0500

    added a button for editing projects, doesnt work as intended for now, also made it so that a project's title is displayed on the header when clicked

--- src/addingProjects.js
+++ src/addingProjects.js
@@ -1,37 +1,100 @@
-export class AddingProjects {
+import {editProjectBool, addButton} from "/src/index.js"
+import {ExtractTextFromEl} from "/src/domManipulation.js"
+class AddingProjects {
 	constructor() {
 		this.navbar = document.querySelector('.navbar');
 		this.textField = document.getElementById('name');
 		this.form = document.querySelector('.project-form');
 
 		this.projects = new Map(); // store projects
-		this.id = 0; // current id
-	}
-
+        this.id = 0; // current id
+        this.editForm = false;
+        //this.editButton = document.querySelector('.project');
+        
+        this.projectDiv = document.querySelector('.project')
+        this.header = document.querySelector('.header');
+        this.projectName = document.createElement('div');
+    }
+    
 	addProject() {
-		this.projectDiv = document.createElement('div');
+		this.projectDiv = document.createElement('span');
 		this.projectDiv.textContent = this.textField.value;
 		this.projectDiv.classList.add('project');
-
-		if (this.textField.value.length > 0) {
-			this.textField.value = '';
-		}
+		this.textField.value = '';
 
 		this.form.classList.toggle('removed');
 
-		const deleteProject = document.createElement('div');
+        const deleteProject = document.createElement('button');
+        deleteProject.classList.add('delete-project')
+        const editTitle = document.createElement('button');
 
-		this.projects.set(++this.id, deleteProject);
+		this.projects.set(++this.id, deleteProject, editTitle);
 
 		const projectDivCopy = this.projectDiv;
 
 		deleteProject.addEventListener('click', () => {
 			projectDivCopy.remove();
-		});
-
+        });
+        
+        editTitle.addEventListener('click', () => {
+            editProjectBool = true;
+            this.form.classList.remove('removed')
+            document.querySelector('.adding-project').disabled = true;
+        })
+     
 		this.projectDiv.appendChild(deleteProject);
-		deleteProject.textContent = 'X';
+        deleteProject.textContent = 'X';
+        this.projectDiv.appendChild(editTitle)
+        editTitle.textContent = 'E' //make this a button not a text content
+        editTitle.classList.add('edit-project');
 
-		this.navbar.appendChild(projectDivCopy);
-	}
+        this.navbar.appendChild(projectDivCopy);
+        
+    }
+
+    showTodo() {
+        this.projectDiv.addEventListener('click', () => {
+            this.projectName.textContent = this.projectDiv.textContent.slice(0, -2);;
+            this.projectName.classList.add('navbar-projects')
+            this.header.appendChild(this.projectName);
+    })
+    }
+     editProject() {
+                if (editProjectBool === true) {
+                    this.projectDiv.textContent = this.textField.value  
+                this.form.classList.add('removed');
+                this.textField.value = '';
+                
+                // this.projectDiv.textContent = this.textField.value  
+                // this.form.classList.add('removed');
+                // this.textField.value = '';
+
+                const deleteProject = document.createElement('button');
+                deleteProject.classList.add('delete-project')
+                const editTitle = document.createElement('button');
+
+                //const projectDivCopy = this.projectDiv;
+
+                deleteProject.addEventListener('click', () => {
+                    this.projectDiv.remove();
+                });
+                editTitle.addEventListener('click', () => {
+                    editProjectBool = true;
+                    this.form.classList.remove('removed')
+                    document.querySelector('.adding-project').disabled = true;            
+                    
+                })
+                this.projectDiv.appendChild(deleteProject);
+                deleteProject.textContent = 'X';
+                this.projectDiv.appendChild(editTitle)
+                editTitle.textContent = 'E'
+                editTitle.classList.add('edit-project');
+                this.navbar.appendChild(this.projectDiv);
+            
+            }
+        }   
 }
+
+const addingProjects = new AddingProjects()
+
+export {addingProjects}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { todos } from "/src/todos.js";
 //import {openNav, sideNav, navbar} from "/src/domManipulation.js"
 import {domManip } from "/src/domManipulation.js"
-import {AddingProjects} from "/src/addingProjects.js"
-const addingProjects = new AddingProjects()
+import {addingProjects} from "/src/addingProjects.js"
+
+
+let editProjectBool = false;
 
 const sideNav = document.querySelector('.openNav');
 const navbar = document.querySelector('.navbar');
@@ -15,9 +17,47 @@ sideNav.addEventListener('click', () => {
     domManip.addProject();
 })
 
-
 domManip.formClose();
 
+
+
+const projectName = document.createElement('div');
+const header = document.querySelector('.header')
+
+
+
 addButton.addEventListener('click', () => {
-    addingProjects.addProject();
+    
+    if (editProjectBool === false) {
+        addingProjects.addProject();
+   
+    }
+    if (editProjectBool === true) {
+        addingProjects.editProject()
+    }
+    //editProjectBool = false;
+    document.querySelector('.adding-project').disabled = false;
 })
+
+navbar.addEventListener('click', (e) => {
+    const projectDiv = document.querySelectorAll('.project')
+    if (e.target.tagName !== 'SPAN') return
+    
+    //let emptyArr = []
+    projectDiv.forEach(div => div.addEventListener('click', () => {
+        projectName.textContent = div.textContent.slice(0, -2);
+    }))
+        
+    //.slice(0, -2);
+    projectName.classList.add('navbar-projects')
+    header.appendChild(projectName);
+    
+})
+
+// if (editProjectBool === true) {
+//     console.log(editProjectBool, 'from editing proj');
+//     addingProjects.editProject()
+// }
+
+export {editProjectBool, addButton}
+
